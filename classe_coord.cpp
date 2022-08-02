e7341f70211e40f9224a5c5724985eca118fe63a
Rafael Nardi <nardi.rafael@gmail.com>
2022-08-01 22:45:15 -0300

    classe coord renomeada para vetor3d. construtor, método de reset e método de soma de vetores criados. Próxima etapa: criar método que pega coordenadas esféricas (r, theta, phi) --- ângulos em radianos e retorna vetor em coordenadas cartesianas

--- classe_coord.cpp
+++ classe_coord.cpp
@@ -14,33 +14,39 @@ Status:FUNCIONANDO
 #include <iostream>
 
 
-
-  
- //Classe que recebe as coordenadas do vetor S
-  class Coord{
-    //coordenadas
-   float x,y,z;
-   
-   private: 
-   float vector3d[3];
-   
-   public:
-   void setVerctor3d(float,float,float);
+//Classe que recebe as coordenadas do vetor S
+class vetor3d{
  
+ public: 
+ float coord[3];
+
+ vetor3d(float x, float y, float z){
+	this->coord[0] = x;
+	this->coord[1] = y;
+	this->coord[2] = z;
+ }
+
+ 	void reset_coord(float x, float y, float z){
+	this->coord[0] = x;
+	this->coord[1] = y;
+	this->coord[2] = z;
+}
+
+vetor3d vectorsum(vetor3d v, vetor3d result){
+result.coord[0] = this->coord[0] + v.coord[0];
+result.coord[1] = this->coord[1] + v.coord[1];
+result.coord[2] = this->coord[2] + v.coord[2];
+
+return result;
+
+}
+
+//esta função deve fazer a transformação de coordenadas esféricas (r, theta, phi) para cartesianas
+//observe que os ângulos devem estar em radianos
+	void vetor_3d::transf_coord_from_spher_to_cart(float r, float theta_rad, float phi_rad){
+		this->coord[0] = 
+		this->coord[1] = 
+		this->coord[2] = 
+	}
+
 };
-//método para acessar o vertor
- void Coord::setVerctor3d(float x,float y,float z){
-     vector3d[0]= {x};
-     vector3d[1]= {y};
-     vector3d[2]= {z};
-
-}
-
-int main(){
-
- Coord coordenadas; //criando objeto
-coordenadas.setVerctor3d(0,0,0);//Entrada de coordenadas para o vetor
-
-
-	return 0;
-}
