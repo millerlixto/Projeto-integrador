0e4810ce05827ba98dad1f060b876c287cc2ae07
Rafael Nardi <nardi.rafael@gmail.com>
2022-08-01 23:04:41 -0300

    construtor trivial criado. Indicativo do método que calcula o produto escalar entre dois vetores. tbm fica como tarefa

--- classe_coord.cpp
+++ classe_coord.cpp
@@ -20,6 +20,14 @@ class vetor3d{
  public: 
  float coord[3];
 
+ //construtor trivial ... pode ser útil em algum contexto
+ vetor3d(){
+	this->coord[0] = 0;
+	this->coord[1] = 0;
+	this->coord[2] = 0;
+ }
+
+ //construtor já com coordenadas
  vetor3d(float x, float y, float z){
 	this->coord[0] = x;
 	this->coord[1] = y;
@@ -38,12 +46,20 @@ result.coord[1] = this->coord[1] + v.coord[1];
 result.coord[2] = this->coord[2] + v.coord[2];
 
 return result;
+}
 
+//método que calcula o produto escalar entre dois vetores:
+float scalar_product(vetor3d v){
+float result = 0.0;
+
+//código aqui
+
+return result;
 }
 
 //esta função deve fazer a transformação de coordenadas esféricas (r, theta, phi) para cartesianas
 //observe que os ângulos devem estar em radianos
-	void vetor_3d::transf_coord_from_spher_to_cart(float r, float theta_rad, float phi_rad){
+	void transf_coord_from_spher_to_cart(float r, float theta_rad, float phi_rad){
 		this->coord[0] = 
 		this->coord[1] = 
 		this->coord[2] = 
