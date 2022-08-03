5a5140d0630de00c734b7d005b145ca07da196b0
millerlixto <millerlixto@gmail.com>
2022-08-03 17:37:36 -0300

    modificação da extensão do arquivo classe_vector3D.cpp para .h, para que possa mandar no main.cpp um exemplo de ultilzação chamando as funções

--- main.cpp
+++ main.cpp
@@ -8,72 +8,23 @@ O programa retorna o vetor S em coordenadas locais/horizontais a partir das coor
 Criado por: Miller calixto almeida
 Data de criação: 24/07/2022
 
-Ultima modificacao: 27/07/2022
-Status:EM ANDAMENTO
+Ultima modificacao: 03/01/2022
+Status:FUNCIONA
 * *******************************************************************************/
+
+#include "classe_vetor3D.h"
 #include <cmath>
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 
-class Coord{
-  
-  public:
-     void vetor3d( float x, float y,float z){
-  
-      std::vector<float> array_coord;
-      
-      array_coord.push_back (x);
-      array_coord.push_back (y);
-      array_coord.push_back (z);
-    }
-   
-};
 
 int main(){
 
- 
 
-//
-//Variaves das coordenadas
-	double altura,anguloAzimultal;//coordenadas locais
-	double anguloHorario,delta;//Coordenadas equatoriais
-	double theta;//rotação em torno do eixo cartesiano y
-	
-//abertura de arquivo
-    FILE *arq1;
+vetor3d coordenadas; //criando objeto;
+coordenadas.reset_coord(0,0,0);//Entrada de coordenadas para o vetor
 
-      arq1=fopen("vetor_S.dat","w"); //Abertura de arquivo
-
-
-       //Parametros da função
-       altura=0.0;
-       anguloAzimultal = 0.0;
-       anguloHorario = 0.0;
-       delta = 0.0;
-       theta = 0.0;
-     
-//função 1
-double cos_altura = (std::cos(theta)*std::cos(delta)*std::cos(anguloHorario)- std::sin(theta)*std::sin(delta))/std::cos(anguloAzimultal);
-
-//função 2
-double cos_angAzimultal = (std::cos(theta)*std::cos(delta)*std::cos(anguloHorario)- std::sin(theta)*std::sin(delta))/std::cos(altura);
-
-//função 3
-double sin_angAzimultal = (std::cos(delta)*std::sin(anguloHorario))/std::cos(altura);
-
-//função 4
-double sin_altura = std::sin(theta)*std::sin(delta) + std::cos(theta)*std::cos(delta)*std::cos(anguloHorario);
-
-
-
-
-
-	fprintf(arq1,"%lf",cos_altura); //gravação de arquivo
-
-std::cout<<cos_altura<<","<<cos_angAzimultal<<","<<sin_angAzimultal<<","<<sin_altura;
-   
-fclose(arq1); //Fechamento do arquivo
 
 	return 0;
 }
