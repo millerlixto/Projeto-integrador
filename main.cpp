bd99b583ac8cb49fba46ee4404635505827e9bf1
Rafael Nardi <nardi.rafael@gmail.com>
2022-08-04 09:08:38 -0300

    teste de método de soma realizado. Próxima etapa: construir método que faz a transformação de coordenadas do vetor

--- main.cpp
+++ main.cpp
@@ -21,13 +21,25 @@ Status:FUNCIONA
 
 int main(){
 
-vetor3d coordenadas; //criando objeto;
-coordenadas.reset_comp(0,0,0);//Entrada de coordenadas para o vetor
+vetor3d v1; //criando objeto;
+v1.reset_comp(1,0,0);//Entrada de coordenadas para o vetor
 
-std::cout << "Valores das componentes do vetor:" << std::endl;
-std::cout << "Vx="<< coordenadas.comp[0] << std::endl;
-std::cout << "Vy="<< coordenadas.comp[1] << std::endl;
-std::cout << "Vz="<< coordenadas.comp[2] << std::endl;
+std::cout << "Valores das componentes do vetor v1:" << std::endl;
+std::cout << "v1_x="<< v1.comp[0] << std::endl;
+std::cout << "v1_y="<< v1.comp[1] << std::endl;
+std::cout << "v1_z="<< v1.comp[2] << std::endl;
+
+vetor3d v2(1,1,1);
+
+vetor3d result;//criando o objeto assim ele já recebe 0 nas componentes pelo construtor trivial
+
+result = v1.vectorsum(v2, result);
+
+std::cout << "Valores das componentes do vetor soma:" << std::endl;
+std::cout << "result_x="<< result.comp[0] << std::endl;
+std::cout << "result_y="<< result.comp[1] << std::endl;
+std::cout << "result_z="<< result.comp[2] << std::endl;
+
 
 
 	return 0;
